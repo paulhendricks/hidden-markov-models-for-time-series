c82549969a23a351bac6b549f4d2618bba6027c4
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-12 16:41:54 -0500

    examples

--- ch01/examples.R
+++ ch01/examples.R
@@ -1,1 +1,19 @@
-# Example 1.1
+# Load libraries
+library(depmixS4)
+
+# Load data
+earthquakes <- read.csv("../data/earthquakes.csv", 
+                        stringsAsFactors = FALSE)
+
+# Table 1.2
+model_earthquakes <- function(.m, ...) {
+  .model <- depmix(response = count ~ 1, data = earthquakes, 
+                   nstates = .m, trstart = runif(.m ** 2), ...)
+  return(fit(.model))
+}
+
+set.seed(1)
+fits <- lapply(1:4, model_earthquakes)
+
+
+
