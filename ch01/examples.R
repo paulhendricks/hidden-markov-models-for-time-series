45dff002126ad0280cd7397cc0d8e7a9f0609772
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-13 11:47:00 -0500

    moved examples out

--- ch01/examples.R
+++ ch01/examples.R
@@ -31,3 +31,16 @@ pois_HMM_mle(x = earthquakes$count, m = 3,
 pois_HMM_mle(x = earthquakes$count, m = 4, 
             lambda0 =  c(11.283, 13.853, 19.695, 29.700), 
             gamma0 = r_gamma(4)) # Trouble reproducing textbook results
+
+
+# Example
+m <- 2
+x <- sample(c(rpois(100, 5), rpois(100, 30)), replace = FALSE)
+l0 <- runif(m)
+g0 <- matrix(runif(m ** 2), nrow = m, ncol = m, byrow = TRUE)
+
+pois_HMM_mle(x = x, m = m, lambda0 = l0, gamma0 = g0)
+
+g <- r_gamma(m)
+
+pois_HMM_generate_sample(n = 100, m = 2, lambda = c(2, 500), gamma = g)
